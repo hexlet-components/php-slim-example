92051c6cdccdcd3e64b6f94135b9b486c3339fb3
Roman Ashikov <rmn.shkv@gmail.com>
2021-10-13 15:01:31 +0700

    change config

--- public/index.php
+++ public/index.php
@@ -3,9 +3,18 @@
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
 
-$app = AppFactory::create();
+// Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если вам интересно, то посмотрите DI Container
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -26,6 +35,14 @@ $app->post('/users', function ($request, $response) {
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
