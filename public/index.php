21ab1cc0fb6e531d2607baceaef99b4ce1824984
Roman Ashikov <rmn.shkv@gmail.com>
2021-10-19 13:34:53 +0700

    add users edit

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если вам интересно, то посмотрите DI Container
 use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
 use App\Validator;
 
@@ -47,6 +48,7 @@ $app = AppFactory::createFromContainer($container);
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
@@ -116,12 +118,60 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($users) {
         return $response->write('Page not found')->withStatus(404);
     }
 
+    $messages = $this->get('flash')->getMessages();
+
     $params = [
         'id' => $id,
-        'nickname' => $users[$id]['nickname'],
+        'user' => $users[$id],
+        'flash' => $messages
     ];
 
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('users.show');
+
+$app->get('/users/{id}/edit', function ($request, $response, $args) use ($users) {
+        $messages = $this->get('flash')->getMessages();
+        $id = $args['id'];
+        $userData = $users[$id];
+        $params = [
+            'id' => $id,
+            'userData' => $userData,
+            'errors' => [],
+            'flash' => $messages
+        ];
+
+        return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+    }
+)->setName('users.edit');
+
+$app->patch('/users/{id}', function ($request, $response, $args) use ($users, $router) {
+        $id = $args['id'];
+        $user = $users[$id];
+        $userData = $request->getParsedBodyParam('user');
+
+        $validator = new Validator();
+        $errors = $validator->validate($userData);
+
+        if (count($errors) === 0) {
+            $user['nickname'] = $userData['nickname'];
+            $user['email'] = $userData['email'];
+            $users[$id] = $user;
+
+            saveUsers(FILE_PATH, $users);
+
+            $this->get('flash')->addMessage('success', "User was updated successfully");
+
+            return $response->withRedirect($router->urlFor('users.show', $args));
+        }
+
+        $params = [
+            'id' => $id,
+            'userData' => $userData,
+            'errors' => $errors
+        ];
+
+        return $this->get('renderer')->render($response->withStatus(422), 'users/edit.phtml', $params);
+    }
+);
 
 $app->run();
