df9bfc5098ff0c6793adae8ad0620e0d71de9775
Roman Ashikov <rmn.shkv@gmail.com>
2021-10-20 18:34:50 +0700

    add authentication

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ use App\Validator;
 session_start();
 
 const FILE_PATH = __DIR__ . '/../users.json';
+const ADMIN_EMAIL = 'admin@hexlet.io';
 
 function getUsers($request)
 {
@@ -40,10 +41,46 @@ $app->addErrorMiddleware(true, true, true);
 $app->add(MethodOverrideMiddleware::class);
 
 $app->get('/', function ($request, $response) use ($router) {
-    return $response->withRedirect($router->urlFor('users.index'), 302);
+    if (isset($_SESSION['isAdmin'])) {
+        return $response->withRedirect($router->urlFor('users.index'));
+    }
+
+    $messages = $this->get('flash')->getMessages();
+    $params = [
+        'email' => '',
+        'flash' => $messages ?? []
+    ];
+
+    return $this->get('renderer')->render($response, 'home.phtml', $params);
 })->setName('home');
 
+$app->post('/login', function ($request, $response) use ($router) {
+    $email = $request->getParsedBodyParam('email');
+
+    if ($email === ADMIN_EMAIL) {
+        $_SESSION['isAdmin'] = true;
+
+        return $response->withRedirect($router->urlFor('users.index'));
+    }
+
+    $this->get('flash')->addMessage('error', 'Access Denied!');
+
+    return $response->withRedirect($router->urlFor('home'));
+});
+
+$app->delete('/logout', function ($request, $response) use ($router) {
+        session_destroy();
+
+        return $response->withRedirect($router->urlFor('home'));
+});
+
 $app->get('/users', function ($request, $response) {
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
+
     $term = $request->getQueryParam('term') ?? '';
     $users = getUsers($request) ?? [];
     $usersList = isset($term) ? filterUsersByName($users, $term) : $users;
@@ -74,7 +111,7 @@ $app->post('/users', function ($request, $response) use ($router) {
 
         $this->get('flash')->addMessage('success', 'User was added successfully');
 
-        return $response->withHeader('Set-Cookie', "users={$encodedUsers}")
+        return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
             ->withRedirect($router->urlFor('users.index'));
     }
 
@@ -87,6 +124,12 @@ $app->post('/users', function ($request, $response) use ($router) {
 })->setName('users.store');
 
 $app->get('/users/new', function ($request, $response) {
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
+
     $params = [
         'userData' => [],
         'errors' => []
@@ -101,6 +144,12 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
 })->setName('courses.show');
 
 $app->get('/users/{id}', function ($request, $response, $args) {
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
+
     $id = $args['id'];
     $users = getUsers($request);
 
@@ -120,65 +169,68 @@ $app->get('/users/{id}', function ($request, $response, $args) {
 })->setName('users.show');
 
 $app->get('/users/{id}/edit', function ($request, $response, $args) {
-        $messages = $this->get('flash')->getMessages();
-        $id = $args['id'];
-        $users = getUsers($request);
-        $userData = $users[$id];
-        $params = [
-            'id' => $id,
-            'userData' => $userData,
-            'errors' => [],
-            'flash' => $messages
-        ];
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
 
-        return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+        return $response->withRedirect($router->urlFor('home'));
     }
-)->setName('users.edit');
+
+    $messages = $this->get('flash')->getMessages();
+    $id = $args['id'];
+    $users = getUsers($request);
+    $userData = $users[$id];
+    $params = [
+        'id' => $id,
+        'userData' => $userData,
+        'errors' => [],
+        'flash' => $messages
+    ];
+
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+})->setName('users.edit');
 
 $app->patch('/users/{id}', function ($request, $response, $args) use ($router) {
-        $id = $args['id'];
-        $users = getUsers($request);
-        $user = $users[$id];
-        $userData = $request->getParsedBodyParam('user');
+    $id = $args['id'];
+    $users = getUsers($request);
+    $user = $users[$id];
+    $userData = $request->getParsedBodyParam('user');
 
-        $validator = new Validator();
-        $errors = $validator->validate($userData);
+    $validator = new Validator();
+    $errors = $validator->validate($userData);
 
-        if (count($errors) === 0) {
-            $user['nickname'] = $userData['nickname'];
-            $user['email'] = $userData['email'];
-            $users[$id] = $user;
-
-            $encodedUsers = json_encode($users);
-
-            $this->get('flash')->addMessage('success', "User was updated successfully");
-
-            return $response->withHeader('Set-Cookie', "users={$encodedUsers}")
-                ->withRedirect($router->urlFor('users.show', $args));
-        }
-
-        $params = [
-            'id' => $id,
-            'userData' => $userData,
-            'errors' => $errors
-        ];
-
-        return $this->get('renderer')->render($response->withStatus(422), 'users/edit.phtml', $params);
-    }
-);
-
-$app->delete('/users/{id}', function ($request, $response, $args) use ($router) {
-        $id = $args['id'];
-        $users = getUsers($request);
-        unset($users[$id]);
+    if (count($errors) === 0) {
+        $user['nickname'] = $userData['nickname'];
+        $user['email'] = $userData['email'];
+        $users[$id] = $user;
 
         $encodedUsers = json_encode($users);
 
-        $this->get('flash')->addMessage('success', "User was deleted successfully");
+        $this->get('flash')->addMessage('success', "User was updated successfully");
 
-        return $response->withHeader('Set-Cookie', "users={$encodedUsers}")
-            ->withRedirect($router->urlFor('users.index'));
+        return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
+            ->withRedirect($router->urlFor('users.show', $args));
     }
-);
+
+    $params = [
+        'id' => $id,
+        'userData' => $userData,
+        'errors' => $errors
+    ];
+
+    return $this->get('renderer')->render($response->withStatus(422), 'users/edit.phtml', $params);
+});
+
+$app->delete('/users/{id}', function ($request, $response, $args) use ($router) {
+    $id = $args['id'];
+    $users = getUsers($request);
+    unset($users[$id]);
+
+    $encodedUsers = json_encode($users);
+
+    $this->get('flash')->addMessage('success', "User was deleted successfully");
+
+    return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
+        ->withRedirect($router->urlFor('users.index'));
+});
 
 $app->run();
